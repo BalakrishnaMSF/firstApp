c1b4f1e687a14c1bdedb228786b3963e41f687a1
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-04-10 14:53:32 +0530

    I used all the life cycles of the activity

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("lifecycle","onCreate");
     }
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart");
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("lifecycle","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy");
     }
 }
