d7823276a515aff310bd9dbd41a78f3be251db27
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-04-11 18:02:01 +0530

    I have changed the java code for login

---
+++ app/src/main/java/com/example/myfirstapplication/Constant.java
@@ -0,0 +1,12 @@
+package com.example.myfirstapplication;
+
+public class Constant {
+
+
+    public static String USERNAME_EMPTY = "User name shouldn't be empty";
+    public static String WRONG_USERNAME = "Wrong User Name ";
+    public static String WRONG_PASSWORD = "Wrong Password ";
+    public static String LOGIN = "Logged";
+
+
+}
--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -6,39 +6,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
-    Button b1;
-    EditText ed1,ed2;
-    LinearLayout l1;
-    TextView tx1,tx2;
+    Button login;
+    EditText username,password;
+    String regex_username="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tx1 = (TextView)findViewById(R.id.hello);
-        l1=(LinearLayout) findViewById(R.id.login_linear);
-        ed1 = (EditText)findViewById(R.id.username);
-        ed2 = (EditText)findViewById(R.id.password);
-        b1 = (Button)findViewById(R.id.login);
-        tx2 = (TextView)findViewById(R.id.forget);
-        b1.setOnClickListener(new View.OnClickListener() {
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        login = (Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ed1.getText().toString().equals("bala@gmail.com") &&
-                        ed2.getText().toString().equals("bala123")) {
+                if(isEmpty(username)){
                     Toast.makeText(getApplicationContext(),
-                            "Redirecting to the page", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "Wrong username and password", Toast.LENGTH_SHORT).show();
-                    tx2.setVisibility(View.VISIBLE);
+                            Constant.WRONG_USERNAME, Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                finish();
+                if(isEmpty(password)){
+                    Toast.makeText(getApplicationContext(),
+                            Constant.WRONG_PASSWORD, Toast.LENGTH_SHORT).show();
+                }
+                if(isValid(username)){
+                    if(password.getText().length()>=8){
+                        Toast.makeText(getApplicationContext(),
+                                Constant.LOGIN, Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         })
     ;}
+    boolean isEmpty(EditText editText){
+        return editText == null || editText.getText() == null || editText.getText().toString().isEmpty();
+    }
+    boolean isValid(EditText editText){
+        Pattern pattern = Pattern.compile(regex_username);
+        Matcher matcher = pattern.matcher(username.getText());
+        return matcher.matches();
+    }
+
 }
