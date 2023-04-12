e9f9a1f8139b3f281aef2c4a7c90c8415378b898
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-04-12 18:13:47 +0530

    I used the shared Preferences for login page

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -2,21 +2,38 @@ package com.example.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username1,password1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username1=findViewById(R.id.username);
+        password1=findViewById(R.id.password);
 
     }
-
-
     @Override
     protected void onResume() {
         super.onResume();
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        String user_name=sh.getString("Username","");
+        String password=sh.getString("Password","");
+        username1.setText(user_name);
+        password1.setText(password);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+        myEdit.putString("username", username1.getText().toString());
+        myEdit.putString("password", password1.getText().toString());
+        myEdit.apply();
     }
 }
