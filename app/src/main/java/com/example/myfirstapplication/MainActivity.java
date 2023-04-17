bdb4887d9d65cc8a4302565b126e32ca9317a898
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-04-17 18:19:53 +0530

    I have done fragment implementation

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loginButton.setVisibility(View.GONE);
+                Fragment fragment=new FragmentOne();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.add(R.id.frameLayout,fragment).commit();
+            }
+        });
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
 }
