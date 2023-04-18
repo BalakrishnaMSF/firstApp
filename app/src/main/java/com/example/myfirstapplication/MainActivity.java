4eae4d0ae878bcedf5fd69e4ea89f02332869134
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-04-18 11:40:36 +0530

    I used the alert dialogue in the implementation

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.myfirstapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +30,20 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.add(R.id.frameLayout,fragment).commit();
             }
         });
+
     }
-
-
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Do you want to exit ?");
+        builder.setTitle("Alert !");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", (DialogInterface.OnClickListener) (dialog, which) -> {
+        });
+        builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 }
