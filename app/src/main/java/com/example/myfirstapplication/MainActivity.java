d4c702a638a57a87e384d334369d564709f82415
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-04-18 14:35:45 +0530

    I used the bottom sheet dialogue in the implementation

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -3,20 +3,56 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
+    Button show;
+    BottomSheetDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        show = findViewById(R.id.show);
+
+        dialog = new BottomSheetDialog(this);
+
+        createDialog();
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+
+        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+
     }
 
+    private void createDialog() {
+        View view = getLayoutInflater().inflate(R.layout.bottom_dialog,null,false);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+        Button submit = view.findViewById(R.id.submit);
+        EditText name = view.findViewById(R.id.name);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,name.getText().toString(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        dialog.setContentView(view);
     }
+
 }
